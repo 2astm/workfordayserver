ba978fea51fabd75febe75d5f9bc4af869dce3b8
Lera <dulevets.v@gmail.com>
2018-11-12 01:26:37 +0200

    Added UserController, Added some methods to UserRepository and UserService

--- src/main/java/com/no/company/workfordayserver/repos/UserRepository.java
+++ src/main/java/com/no/company/workfordayserver/repos/UserRepository.java
@@ -3,5 +3,42 @@ package com.no.company.workfordayserver.repos;
 
 import com.no.company.workfordayserver.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import javax.transaction.Transactional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByLogin(String login);
+
+    @Modifying
+    @Transactional
+    @Query ("Update User set user.password = ?2 where id ?= 1")
+    public void setPassword(@Param(value = "user_id") long id, @Param(value = "new_password") String password);
+
+    @Modifying
+    @Transactional
+    @Query ("Update User set user.login = ?2 where id ?= 1")
+    public void setLogin(@Param(value = "user_id") long id, @Param(value = "new_login") String login);
+
+    @Modifying
+    @Transactional
+    @Query ("Update User set user.name = ?2 where id ?= 1")
+    public void setName(@Param(value = "user_id") long id, @Param(value = "new_name") String name);
+
+    @Modifying
+    @Transactional
+    @Query ("Update User set user.Surname = ?2 where id ?= 1")
+    public void setSurname(@Param(value = "user_id") long id, @Param(value = "Surname") String Surname);
+
+    @Modifying
+    @Transactional
+    @Query ("Update User set user.email = ?2 where id ?= 1")
+    public void setEmail(@Param(value = "user_id") long id, @Param(value = "email") String email);
+
+    @Modifying
+    @Transactional
+    @Query ("Update User set user.phoneNumber = ?2 where id ?= 1")
+    public void setPhoneNumber(@Param(value = "user_id") long id, @Param(value = "phoneNumber") String phoneNumber);
+}
--- src/main/java/com/no/company/workfordayserver/rest/UserController.java
+++ src/main/java/com/no/company/workfordayserver/rest/UserController.java
@@ -5,11 +5,13 @@ import com.no.company.workfordayserver.entities.Dispute;
 import com.no.company.workfordayserver.entities.User;
 import com.no.company.workfordayserver.entities.Vacancy;
 import com.no.company.workfordayserver.services.DisputeService;
+import com.no.company.workfordayserver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.crypto.Data;
+import java.lang.ref.ReferenceQueue;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,5 +22,30 @@ public class UserController {
     @RequestMapping(value = "/test")
     public String test(){
         return "Hello world";
+    }
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void addUser(@RequestBody User user) {
+        if (user.getLogin() != null && (user.getPassword() != null)) {
+            user.setDateRegister(new Date());
+            user.setDateUpdate(user.getDateRegister());
+            service.addUser(user);
+        }
+    }
+
+    @RequestMapping(value = "/change,", method = RequestMethod.PUT)
+    public void changeUser(@RequestBody User user, @RequestHeader(name = "login") String login, @RequestHeader(name = "password") String password) {
+        User oldUser = service.getUserByLogin(login);
+        if (oldUser.getPassword().equals(password)) {
+            service.updateUser(oldUser.getId(), user);
+        } 
+    }
+
+    @RequestMapping(value = "/remove", method = RequestMethod.DELETE)
+    public void remove(@RequestBody User user) {
+        service.remove(user);
     }
 }
